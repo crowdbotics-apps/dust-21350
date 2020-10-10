3308ddb705527c7e6266f55939f31f93568f8d72
Crowdbotics <team@crowdbotics.com>
2020-10-10 22:04:40 +0000

    Update models

--- backend/dust_21350/wsgi.py
+++ backend/dust_21350/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dust_21350.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dust_21350.settings")
 
 application = get_wsgi_application()
